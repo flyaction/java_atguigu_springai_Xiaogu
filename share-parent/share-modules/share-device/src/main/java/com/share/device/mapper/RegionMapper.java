9deab8344f3ef2d5ecafdcb2d80af3b78db3d1e0
action <xingdong1117@126.com>
2025-12-16 15:37:17 +0800

    feat:地区管理

---
+++ share-parent/share-modules/share-device/src/main/java/com/share/device/mapper/RegionMapper.java
@@ -0,0 +1,10 @@
+package com.share.device.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.share.device.domain.Region;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface RegionMapper extends BaseMapper<Region> {
+
+}
---
+++ share-parent/share-modules/share-device/src/main/java/com/share/device/service/IRegionService.java
@@ -0,0 +1,16 @@
+package com.share.device.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.share.device.domain.Region;
+
+import java.util.List;
+
+/**
+ * @author: action
+ * @create: 2025/12/16 15:29
+ **/
+public interface IRegionService extends IService<Region> {
+
+    List<Region> treeSelect(String parentCode);
+
+}
