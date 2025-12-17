80c8dcd212144b953bf468d7b689e7871a721dcc
action <xingdong1117@126.com>
2025-12-17 11:04:51 +0800

    feat:优化站点新增修改-使用mongodb

--- share-parent/share-modules/share-device/src/main/java/com/share/device/service/impl/StationServiceImpl.java
+++ share-parent/share-modules/share-device/src/main/java/com/share/device/service/impl/StationServiceImpl.java
@@ -4,14 +4,19 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.share.device.domain.Cabinet;
 import com.share.device.domain.Station;
+import com.share.device.domain.StationLocation;
 import com.share.device.mapper.StationMapper;
+import com.share.device.repository.StationLocationRepository;
 import com.share.device.service.IRegionService;
 import com.share.device.service.IStationService;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,6 +34,9 @@ public class StationServiceImpl extends ServiceImpl<StationMapper, Station> impl
     @Autowired
     private IRegionService regionService;
 
+    @Autowired
+    private StationLocationRepository stationLocationRepository;
+
     @Override
     public List<Station> selectStationList(Station station)
     {
@@ -45,6 +53,30 @@ public class StationServiceImpl extends ServiceImpl<StationMapper, Station> impl
         return list;
     }
 
+//    @Transactional(rollbackFor = Exception.class)
+//    @Override
+//    public int saveStation(Station station) {
+//        String provinceName = regionService.getNameByCode(station.getProvinceCode());
+//        String cityName = regionService.getNameByCode(station.getCityCode());
+//        String districtName = regionService.getNameByCode(station.getDistrictCode());
+//        station.setFullAddress(provinceName + cityName + districtName + station.getAddress());
+//        this.save(station);
+//
+//        return 1;
+//    }
+//
+//    @Transactional(rollbackFor = Exception.class)
+//    @Override
+//    public int updateStation(Station station) {
+//        String provinceName = regionService.getNameByCode(station.getProvinceCode());
+//        String cityName = regionService.getNameByCode(station.getCityCode());
+//        String districtName = regionService.getNameByCode(station.getDistrictCode());
+//        station.setFullAddress(provinceName + cityName + districtName + station.getAddress());
+//        this.updateById(station);
+//
+//        return 1;
+//    }
+
     @Transactional(rollbackFor = Exception.class)
     @Override
     public int saveStation(Station station) {
@@ -54,6 +86,13 @@ public class StationServiceImpl extends ServiceImpl<StationMapper, Station> impl
         station.setFullAddress(provinceName + cityName + districtName + station.getAddress());
         this.save(station);
 
+        //同步站点位置信息到MongoDB
+        StationLocation stationLocation = new StationLocation();
+        stationLocation.setId(ObjectId.get().toString());
+        stationLocation.setStationId(station.getId());
+        stationLocation.setLocation(new GeoJsonPoint(station.getLongitude().doubleValue(), station.getLatitude().doubleValue()));
+        stationLocation.setCreateTime(new Date());
+        stationLocationRepository.save(stationLocation);
         return 1;
     }
 
@@ -66,6 +105,10 @@ public class StationServiceImpl extends ServiceImpl<StationMapper, Station> impl
         station.setFullAddress(provinceName + cityName + districtName + station.getAddress());
         this.updateById(station);
 
+        //同步站点位置信息到MongoDB
+        StationLocation stationLocation = stationLocationRepository.getByStationId(station.getId());
+        stationLocation.setLocation(new GeoJsonPoint(station.getLongitude().doubleValue(), station.getLatitude().doubleValue()));
+        stationLocationRepository.save(stationLocation);
         return 1;
     }
 
