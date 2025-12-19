3e799ed8e1e4d064edc157fd1f47c4074655874d
action <xingdong1117@126.com>
2025-12-19 10:23:50 +0800

    feat: fe-会员管理-优化

--- share-ui/src/views/user/userInfo/index.vue
+++ share-ui/src/views/user/userInfo/index.vue
@@ -1,45 +1,15 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="微信openId" prop="wxOpenId">
-        <el-input
-          v-model="queryParams.wxOpenId"
-          placeholder="请输入微信openId"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="会员昵称" prop="nickname">
-        <el-input
-          v-model="queryParams.nickname"
-          placeholder="请输入会员昵称"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="性别" prop="gender">
-        <el-input
-          v-model="queryParams.gender"
-          placeholder="请输入性别"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="头像" prop="avatarUrl">
-        <el-input
-          v-model="queryParams.avatarUrl"
-          placeholder="请输入头像"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="电话" prop="phone">
-        <el-input
-          v-model="queryParams.phone"
-          placeholder="请输入电话"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+      <el-form-item label="创建时间" style="width: 308px">
+        <el-date-picker
+            v-model="dateRange"
+            value-format="YYYY-MM-DD"
+            type="daterange"
+            range-separator="-"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+        ></el-date-picker>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -50,99 +20,109 @@
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
         <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['user:userInfo:add']"
-        >新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['user:userInfo:edit']"
-        >修改</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['user:userInfo:remove']"
-        >删除</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['user:userInfo:export']"
+            type="warning"
+            plain
+            icon="Download"
+            @click="handleExport"
+            v-hasPermi="['user:userInfo:export']"
         >导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="userInfoList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="主键" align="center" prop="id" />
-      <el-table-column label="微信openId" align="center" prop="wxOpenId" />
-      <el-table-column label="会员昵称" align="center" prop="nickname" />
-      <el-table-column label="性别" align="center" prop="gender" />
-      <el-table-column label="头像" align="center" prop="avatarUrl" />
-      <el-table-column label="电话" align="center" prop="phone" />
-      <el-table-column label="1有效，2禁用" align="center" prop="status" />
-      <el-table-column label="备注" align="center" prop="remark" />
-      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column label="微信openId" prop="wxOpenId" />
+      <el-table-column label="会员昵称" prop="nickname" width="120"/>
+      <el-table-column prop="lastLoginTime" label="最后一次登录时间" width="180"/>
+      <el-table-column prop="status" label="状态" #default="scope" width="100">
+        {{ scope.row.status == 1 ? '正常' : '停用' }}
+      </el-table-column>
+      <el-table-column prop="depositStatus" label="认证状态" #default="scope" width="100">
+        {{ scope.row.depositStatus == 0 ? '未认证' : scope.row.depositStatus == 1 ? '免押金' : '已交押金' }}
+      </el-table-column>
+      <el-table-column label="注册时间" prop="createTime" width="180"/>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="100">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['user:userInfo:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['user:userInfo:remove']">删除</el-button>
+          <el-button link type="primary" @click="handleShow(scope.row.id)" v-hasPermi="['user:userInfo:query']">详情</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
+
     <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
+        v-show="total>0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
     />
 
     <!-- 添加或修改用户对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="userInfoRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="微信openId" prop="wxOpenId">
-          <el-input v-model="form.wxOpenId" placeholder="请输入微信openId" />
-        </el-form-item>
-        <el-form-item label="会员昵称" prop="nickname">
-          <el-input v-model="form.nickname" placeholder="请输入会员昵称" />
-        </el-form-item>
-        <el-form-item label="性别" prop="gender">
-          <el-input v-model="form.gender" placeholder="请输入性别" />
-        </el-form-item>
-        <el-form-item label="头像" prop="avatarUrl">
-          <el-input v-model="form.avatarUrl" placeholder="请输入头像" />
-        </el-form-item>
-        <el-form-item label="电话" prop="phone">
-          <el-input v-model="form.phone" placeholder="请输入电话" />
-        </el-form-item>
-        <el-form-item label="删除标志" prop="delFlag">
-          <el-input v-model="form.delFlag" placeholder="请输入删除标志" />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" placeholder="请输入备注" />
-        </el-form-item>
+    <el-dialog :title="title" v-model="open" width="70%" append-to-body>
+      <el-form ref="userInfoRef" :model="form" label-width="140px">
+        <el-divider />
+        <span style="margin-bottom: 5px;">基本信息</span>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="头像">
+              <img :src="form.avatarUrl" width="50" />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="昵称">
+              {{ form.nickname }}
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="微信标识">
+              {{ form.wxOpenId }}
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="性别">
+              {{ form.sex == 1 ? '女' : '男' }}
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="最后一次登录ip">
+              {{ form.lastLoginIp }}
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="最后一次登录时间">
+              {{ form.lastLoginTime }}
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="状态">
+              {{ form.status == 1 ? '正常' : '停用' }}
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="创建时间">
+              {{ form.createTime }}
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="认证状态">
+              {{ form.depositStatus == 0 ? '未认证' : form.depositStatus == 1 ? '免押金' : '已交押金' }}
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+
+          </el-col>
+        </el-row>
+
       </el-form>
       <template #footer>
         <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
           <el-button @click="cancel">取 消</el-button>
         </div>
       </template>
@@ -151,7 +131,7 @@
 </template>
 
 <script setup name="UserInfo">
-import { listUserInfo, getUserInfo, delUserInfo, addUserInfo, updateUserInfo } from "@/api/user/userInfo";
+import { listUserInfo, getUserInfo } from "@/api/user/userInfo";
 
 const { proxy } = getCurrentInstance();
 
@@ -159,49 +139,24 @@ const userInfoList = ref([]);
 const open = ref(false);
 const loading = ref(true);
 const showSearch = ref(true);
-const ids = ref([]);
-const single = ref(true);
-const multiple = ref(true);
 const total = ref(0);
 const title = ref("");
+const dateRange = ref([]);
 
 const data = reactive({
   form: {},
   queryParams: {
     pageNum: 1,
     pageSize: 10,
-    wxOpenId: null,
-    nickname: null,
-    gender: null,
-    avatarUrl: null,
-    phone: null,
-    status: null,
   },
-  rules: {
-    wxOpenId: [
-      { required: true, message: "微信openId不能为空", trigger: "blur" }
-    ],
-    gender: [
-      { required: true, message: "性别不能为空", trigger: "blur" }
-    ],
-    createTime: [
-      { required: true, message: "创建时间不能为空", trigger: "blur" }
-    ],
-    updateTime: [
-      { required: true, message: "更新时间不能为空", trigger: "blur" }
-    ],
-    delFlag: [
-      { required: true, message: "删除标志不能为空", trigger: "blur" }
-    ],
-  }
 });
 
-const { queryParams, form, rules } = toRefs(data);
+const { queryParams, form } = toRefs(data);
 
 /** 查询用户列表 */
 function getList() {
   loading.value = true;
-  listUserInfo(queryParams.value).then(response => {
+  listUserInfo(proxy.addDateRange(queryParams.value, dateRange.value)).then(response => {
     userInfoList.value = response.rows;
     total.value = response.total;
     loading.value = false;
@@ -214,26 +169,6 @@ function cancel() {
   reset();
 }
 
-// 表单重置
-function reset() {
-  form.value = {
-    id: null,
-    wxOpenId: null,
-    nickname: null,
-    gender: null,
-    avatarUrl: null,
-    phone: null,
-    status: null,
-    createTime: null,
-    createBy: null,
-    updateTime: null,
-    updateBy: null,
-    delFlag: null,
-    remark: null
-  };
-  proxy.resetForm("userInfoRef");
-}
-
 /** 搜索按钮操作 */
 function handleQuery() {
   queryParams.value.pageNum = 1;
@@ -242,65 +177,18 @@ function handleQuery() {
 
 /** 重置按钮操作 */
 function resetQuery() {
+  dateRange.value = [];
   proxy.resetForm("queryRef");
   handleQuery();
 }
 
-// 多选框选中数据
-function handleSelectionChange(selection) {
-  ids.value = selection.map(item => item.id);
-  single.value = selection.length != 1;
-  multiple.value = !selection.length;
-}
-
-/** 新增按钮操作 */
-function handleAdd() {
-  reset();
-  open.value = true;
-  title.value = "添加用户";
-}
-
 /** 修改按钮操作 */
-function handleUpdate(row) {
-  reset();
-  const _id = row.id || ids.value
-  getUserInfo(_id).then(response => {
+function handleShow(id) {
+  getUserInfo(id).then(response => {
     form.value = response.data;
     open.value = true;
-    title.value = "修改用户";
+    title.value = "详情";
   });
-}
-
-/** 提交按钮 */
-function submitForm() {
-  proxy.$refs["userInfoRef"].validate(valid => {
-    if (valid) {
-      if (form.value.id != null) {
-        updateUserInfo(form.value).then(response => {
-          proxy.$modal.msgSuccess("修改成功");
-          open.value = false;
-          getList();
-        });
-      } else {
-        addUserInfo(form.value).then(response => {
-          proxy.$modal.msgSuccess("新增成功");
-          open.value = false;
-          getList();
-        });
-      }
-    }
-  });
-}
-
-/** 删除按钮操作 */
-function handleDelete(row) {
-  const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除用户编号为"' + _ids + '"的数据项？').then(function() {
-    return delUserInfo(_ids);
-  }).then(() => {
-    getList();
-    proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
 }
 
 /** 导出按钮操作 */
