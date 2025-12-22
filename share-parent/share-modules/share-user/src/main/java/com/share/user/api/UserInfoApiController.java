aa3da61a41716f9b8fc6a66f3ed9ad7bbd8267a2
action <xingdong1117@126.com>
2025-12-22 15:04:22 +0800

    feat:微信授权登录-fix

--- share-parent/share-modules/share-user/src/main/java/com/share/user/api/UserInfoApiController.java
+++ share-parent/share-modules/share-user/src/main/java/com/share/user/api/UserInfoApiController.java
@@ -3,6 +3,7 @@ package com.share.user.api;
 import com.share.common.core.domain.R;
 import com.share.common.core.web.controller.BaseController;
 import com.share.common.security.annotation.InnerAuth;
+import com.share.user.domain.UpdateUserLogin;
 import com.share.user.domain.UserInfo;
 import com.share.user.service.IUserInfoService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -17,10 +18,17 @@ public class UserInfoApiController extends BaseController {
     private IUserInfoService userInfoService;
 
     @Operation(summary = "小程序授权登录")
-    @InnerAuth
     @GetMapping("/wxLogin/{code}")
-    public R<UserInfo> wxLogin(@PathVariable String code) {
+    public R<UserInfo> wxLogin(@PathVariable("code") String code) {
+        System.out.println(code);
         return R.ok(userInfoService.wxLogin(code));
     }
 
+    @Operation(summary = "更新用户登录信息")
+    @InnerAuth
+    @PutMapping("/updateUserLogin")
+    public R<Boolean> updateUserLogin(@RequestBody UpdateUserLogin updateUserLogin)
+    {
+        return R.ok(userInfoService.updateUserLogin(updateUserLogin));
+    }
 }
